f31219b672ac1bfb3fade8e17af896f2dc9199fb
Gabriella Martins <gabmcavalheiro641@gmail.com>
2023-04-20 18:06:45 -0300

    Cadastro de saídas e refatoração
    Escrita do cenário de cadastro de saídas e otimização do código

--- cypress/integration/finances.spec.js
+++ cypress/integration/finances.spec.js
@@ -1,15 +1,23 @@
 /// <reference types="cypress" /> 
 
-const { it } = require("mocha");
-
 context('Dev Finances Agilizei', () => {
+
+    // hooks - trechos que executam antes e depois do teste
+    // before -> antes de todos os testes
+    // beforeEach -> antes de cada teste
+    // after -> depois de todos os testes
+    // afterEach -> depois de cada teste
+
+    beforeEach(() => {
+        cy.visit('https://devfinance-agilizei.netlify.app')
+        cy.get('#data-table tbody tr').should('have.length', 0)
+    });
+
     it('Cadastrar entradas', () => {
         // - entender o fluxo manualmente
         // - mapear os elementos que vamos interagir 
         // - descrever as interações com o cypress 
         // - adicionar as asserções que a gente precisa
-
-        cy.visit('https://devfinance-agilizei.netlify.app')
 
         cy.get('#transaction .button').click() // id + classe
         cy.get('#description').type('Salário') // id
@@ -20,6 +28,16 @@ context('Dev Finances Agilizei', () => {
         cy.get('#data-table tbody tr').should('have.length', 1)
     });
 
-    //Cadastrar saídas
+    it('Cadastrar saídas', () => {
+
+        cy.get('#transaction .button').click() 
+        cy.get('#description').type('Mercado')
+        cy.get('[name=amount]').type(-200)
+        cy.get('[type=date]').type('2023-04-19')
+        cy.get('button').contains('Salvar').click()
+
+        cy.get('#data-table tbody tr').should('have.length', 1)
+    });
+
     //Remover entradas e saídas
 });
